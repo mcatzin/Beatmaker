4304eb88deb62cb08ab078f81c50feb2ec04c764
Mario Catzin <catzin21@gmail.com>
2020-07-09 13:13:08 -0700

    Muting sounds

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class DrumKit {
         this.index = 0;
         this.bpm = 150;
         this.isPlaying = null;
+        this.muteBtns = document.querySelectorAll(".mute");
     }
     activePad() {
         this.classList.toggle("active");
@@ -75,6 +76,36 @@ class DrumKit {
                 break;
         }
     }
+    mute(e) {
+        const muteIndex = e.target.getAttribute("data-track");
+        e.target.classList.toggle("active");
+        if (e.target.classList.contains("active")) {
+            switch (muteIndex) {
+                case "0":
+                    this.kickAudio.volume = 0;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 0;
+                    break;
+                case "2":
+                    this.hihatAudio.volume = 0;
+                    break;
+            }
+        } else {
+            switch (muteIndex) {
+                case "0":
+                    this.kickAudio.volume = 1;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 1;
+                    break;
+                case "2":
+                    this.hihatAudio.volume = 1;
+                    break;
+            }
+        }
+
+    }
 }
 
 const drumkit = new DrumKit();
@@ -93,5 +124,11 @@ drumkit.playbtn.addEventListener("click", () => {
 drumkit.selects.forEach(select => {
     select.addEventListener("change", function (e) {
         drumkit.chnageSound(e);
+    })
+})
+
+drumkit.muteBtns.forEach(btn => {
+    btn.addEventListener("click", function (e) {
+        drumkit.mute(e);
     })
 })
